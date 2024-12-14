3294cf07f64282af6646b8042288cb54b9c3007b
ccisrepairroom <ccisrepairroom@gmail.com>
2024-12-14 08:40:50 +0800

    .

---
+++ EcommerceConnection.session.sql
@@ -0,0 +1,20 @@
+INSERT INTO users (
+    id,
+    name,
+    email,
+    email_verified_at,
+    password,
+    remember_token,
+    created_at,
+    updated_at
+  )
+VALUES (
+    'id:bigint',
+    'name:varchar',
+    'email:varchar',
+    'email_verified_at:timestamp',
+    'password:varchar',
+    'remember_token:varchar',
+    'created_at:timestamp',
+    'updated_at:timestamp'
+  );
